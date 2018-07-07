603a3dfb33acabf5cfd1e34f4cda7029fe89f0dd
Brock Fanning <brockfanning@gmail.com>
2018-07-07 08:04:31 -0400

    Skip link_text and url_text fields.

--- scripts/batch/metadata_from_schema.py
+++ scripts/batch/metadata_from_schema.py
@@ -28,7 +28,11 @@ def main():
     ]
     for language in ['en', 'es', 'fr']:
         for row in response:
-            if 'scope' in row['field'] and row['field']['scope'] in scopes:
+            if 'scope' in row['field']:
+                if row['field']['scope'] not in scopes:
+                    continue
+                if '_link_text' in row['name'] or '_url_text' in row['name']:
+                    continue
                 if language == 'en':
                     data[row['name']] = row['field']['label']
                 else:

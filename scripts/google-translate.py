b1df0502c39b63a80cf97a4eeb8cca498ad32680
Brock Fanning <brockfanning@gmail.com>
2019-04-01 09:54:22 -0400

    Add script and Google package

---
+++ scripts/google-translate.py
@@ -0,0 +1,24 @@
+"""
+Use Google to translate a Yaml file of English strings.
+"""
+
+import sys
+import yaml
+from google.cloud import translate
+
+if len(sys.argv) < 3:
+  sys.exit('Provide an input file and a 2-letter abbreviation for the target language.')
+
+input_file = sys.argv[1]
+language = sys.argv[2]
+
+translate_client = translate.Client()
+with open(input_file, 'r') as stream:
+  yamldata = yaml.load(stream)
+
+for key in yamldata:
+  translation = translate_client.translate(yamldata[key], target_language=language)
+  yamldata[key] = translation['translatedText']
+
+with open('output.yml', 'w') as outfile:
+  yaml.dump(yamldata, outfile, default_flow_style=False, allow_unicode=True)
